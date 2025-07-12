9435477e75dc86039520616a749c97e7ff7634fb
jiyoon <rhjiyoon@yonsei.ac.kr>
2025-07-12 21:27:52 +0900

    1(1) - 분할 정복(3-1629, 10830) 해결

--- 1(1)-Python/submission/3_10830.py
+++ 1(1)-Python/submission/3_10830.py
@@ -78,3 +78,30 @@ class Matrix:
 
     def __repr__(self) -> str:
         return "\n".join(" ".join(map(str, row)) for row in self.matrix)
+
+
+from typing import Callable
+import sys
+
+
+"""
+-아무것도 수정하지 마세요!
+"""
+
+
+def main() -> None:
+    intify: Callable[[str], list[int]] = lambda l: [*map(int, l.split())]
+
+    lines: list[str] = sys.stdin.readlines()
+
+    N, B = intify(lines[0])
+    matrix: list[list[int]] = [*map(intify, lines[1:])]
+
+    Matrix.MOD = 1000
+    modmat = Matrix(matrix)
+
+    print(modmat ** B)
+
+
+if __name__ == "__main__":
+    main()
